5f88be4d91fca92fd1a8a6fee860b67c70a90210
BuildTools <unconfigured@null.spigotmc.org>
2018-06-13 16:52:20 -0400

    Added initial template

---
+++ GUI development.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+Name
+    GUI development.py
+
+Purpose
+    This python script is used to develop the calculator GUI module.
+    
+Revision Dates
+    13-JUN-2018 (ALE) [0.1]: Initial Version
+"""
+
+_authors_    = ["Andre Lesnick"]
+_version_    = "0.1"
+_maintainer_ = ""
+
+_status_     = "Development"
